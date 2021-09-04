aa203ad9b638167187bc709e88a5e3551d230e0d
Ragendunaduvalath <90086485+Ragendunaduvalath@users.noreply.github.com>
2021-09-04 12:59:10 +0530

    Day2 assignment submitted

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+class Sum {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        c=a+b;
+        System.out.println("Sum of two numbers= "+c);   
+    }
+    
+}
